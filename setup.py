be98684d0dfef84242418e65262b5f417d67f13e
marouanbouchettoy <uanemaro216@gmail.com>
2026-01-15 13:43:42 +0100

    chore: update configuration

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="bmdb",
     version="0.1.0",
-    author="BM-Framework (Marouan Bouchettoy)",
-    description="BMDB - Minimal schema manager for SQLAlchemy",
-    long_description=long_description,
+    author="Marouan Bouchettoy",
+    author_email="marouanbouchettoy@gmail.com",
+    description="BM Database Framework",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/BM-Framework/bmdb",
     packages=find_packages(),
@@ -17,18 +15,15 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     install_requires=[
-        "click>=8.0.0",
-        "PyYAML>=6.0",
-        "python-dotenv>=0.19.0",
-        "SQLAlchemy>=1.4.0",
-        "psycopg2-binary>=2.9.0",
+        "click==8.3.1",
+        "colorama==0.4.6",
+        "greenlet==3.3.0",
+        "python-dotenv==1.2.1",
+        "PyYAML==6.0.3",
+        "setuptools==80.9.0",
+        "SQLAlchemy==2.0.45",
+        "typing_extensions==4.15.0"
     ],
-    entry_points={
-        "console_scripts": [
-            "bmdb=bmdb.cli:cli",
-        ],
-    },
-    include_package_data=True,
 )
