3be62fe3521a0c4b1eaac23bbe997a0ab204ec80
marouanbouchettoy <uanemaro216@gmail.com>
2026-01-21 16:14:28 +0100

    chore: update configuration

--- bmdb/__init__.py
+++ bmdb/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """BMDB - Minimal schema manager for SQLAlchemy"""
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __author__ = "Marouan Bouchettoy"
 
 from bmdb.cli import main
